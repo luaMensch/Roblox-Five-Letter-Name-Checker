d5cf3cb44ca696e46b1d4e83d34a70b5678a840b
luaMensch <79165975+luaMensch@users.noreply.github.com>
2021-07-17 19:07:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-
 import robloxpy,requests,random,string,datetime,multiprocessing
 from robloxpy import Utils as Utils
 
-f = open('codes.txt', "a+")
+def new_line(file_name, text_to_append):
+    with open(file_name, "a+") as file_object:
+        file_object.seek(0)
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        file_object.write(text_to_append)
 
-def gencode():
+def GetID(Username):
+    r = requests.get(Utils.APIURL + f'users/get-by-username?username={Username}')
+    try:
+        return r.json()['Id']
+    except:
+        return r.json()['errorMessage']
+
+def checkusername():
     while True:
-        code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
-        f.write(f'{code}\n')
-        print(code)
+        name = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
+        function = GetID(name)
+        if function == "User not found":
+            new_line("valid.txt", name)
+            print(name)
 
 if __name__ == "__main__":
-    q=multiprocessing.Queue() # Instance of queue class created
-    processes=[multiprocessing.Process(target=gencode) for i in range(1,35)]
+    q=multiprocessing.Queue()
+    processes=[multiprocessing.Process(target=checkusername) for i in range(1,35)]
     for p in processes:
         p.start()
  
@@ -21,5 +35,3 @@ if __name__ == "__main__":
  
     result = [q.get() for p in processes]
     print(result)
-
-# name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
